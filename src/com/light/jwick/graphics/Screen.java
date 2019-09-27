7a3eb996f816d842c068bf71257155c9d2525c0f
litesam <lightsam126@gmail.com>
2019-09-27 22:41:47 +0530

    Sprites added

--- src/com/light/jwick/graphics/Screen.java
+++ src/com/light/jwick/graphics/Screen.java
@@ -37,7 +37,7 @@ public class Screen {
 				int xx = x + xOffset;
 //				if (xx < 0 || xx >= width) break;
 				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE; // 16 x 16 tiles
-				pixels[x + y * width] = tiles[tileIndex];
+				pixels[x + y * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
 		}
 	}
